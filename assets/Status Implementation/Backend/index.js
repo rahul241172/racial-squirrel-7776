8a3b609ffd96c72350752a90adaf83dc18583281
NehaS101 <solankineha297@gmail.com>
2023-03-29 17:32:40 +0530

    added

--- assets/Status Implementation/Backend/index.js
+++ assets/Status Implementation/Backend/index.js
@@ -1,6 +1,17 @@
 const { connection } = require("./Config/db");
 const express = require('express');
+require("dotenv").config();
+
 const app = express()
 app.use(express.json())
 
 
+app.listen(process.env.port,async()=>{
+    console.log(`server is running on port ${process.env.port}`)
+    try {
+        await connection
+        console.log("connected to db")
+    } catch (error) {
+        console.log({error:error,mssg:"finding error while connecting"})
+    }
+})
