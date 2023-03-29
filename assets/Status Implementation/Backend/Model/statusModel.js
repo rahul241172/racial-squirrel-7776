86fa5f85405344d7c751543e8647038a31560b44
NehaS101 <solankineha297@gmail.com>
2023-03-29 17:42:59 +0530

    added

---
+++ assets/Status Implementation/Backend/Model/statusModel.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const StatusSchema = mongoose.Schema({
+
+    name:String,
+    image:String,
+    about:String,
+    views:[{default:1,type:Number}]
+},{ timestamps: {
+    createdAt: 'created_at' 
+  }})
+
+  const StatusModel = mongoose.model("Status",StatusSchema)
