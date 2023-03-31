e48814cb0014064cd251e4df6ef64dd99c7398a7
NehaS101 <solankineha297@gmail.com>
2023-03-31 17:36:41 +0530

    working on status

---
+++ message/frontend/script/index.js
@@ -0,0 +1,6 @@
+//status implementation
+const status_add = document.querySelector("#status_show").addEventListener("click",myfun)
+function myfun(){
+    console.log("click")
+    // window.location.href="./status.html"
+}
