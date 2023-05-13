2bbd292f8801830fef0f3fae9e1d160647ff3e61
gouravsahuu <gsgourav.sahu@gmail.com>
2023-05-13 14:15:25 +0530

    added logic for manually login

--- backend/Main server/Models/user.model.js
+++ backend/Main server/Models/user.model.js
@@ -3,30 +3,33 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
     email: {
         type: String,
-        required: true
+        
     },
     email_verified: {
         type: Boolean,
-        required: true
+        
     },
     family_name: {
         type: String,
-        required: true
+        
     },
     given_name: {
         type: String,
-        required: true
+        
     },
     name: {
         type: String,
-        required: true
+        
     },
     picture: {
         type: String
     },
     sub: {
         type: String,
-        required: true
+        
+    },
+    password: {
+        type: String
     }
 })
 
--- backend/Main server/Routes/user.routes.js
+++ backend/Main server/Routes/user.routes.js
@@ -1,7 +1,55 @@
 const express = require("express");
 const {UserModel} = require("../Models/user.model");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
 const userRoute = express.Router();
+
+//registering user
+userRoute.post("/register",async(req,res) => {
+    const {name,email,password} = req.body;
+    try{
+        const existingUser = await UserModel.find({email});
+        if(existingUser.length == 0){
+            bcrypt.hash(password, 5, async (err,hash) => {
+                const user = new UserModel({name,email,password:hash});
+                await user.save();
+                res.send({"message":"User registered Successfully"});
+            })
+        }
+        else{
+            res.send({"message":"User already exists"});
+        }
+    }
+    catch(err){
+        res.send({"message":"Something Went Wrong","error":err.message});
+    }
+})
+
+//user login
+userRoute.post("/login", async(req,res) => {
+    const userDetail = req.body;
+     try{
+        const user = await UserModel.find({email:userDetail.email});
+        if(user.length > 0){
+            bcrypt.compare(userDetail.password, user[0].password, (err,result) => {
+                if(result){
+                    const token = jwt.sign({userID:user[0]._id},"somekey",{expiresIn: '1h'});
+                    res.send({"message":"Log-In Success","token":token,"help":"You can use this token to access protected routes","username":`${user[0].name}`,"userID":`${user[0]._id}`});
+                }
+                else{
+                    res.send({"message":"Login Failed, Invalid Credentials"});
+                }
+            })
+        }
+        else{
+            res.send({"message":"Login Failed, Invalid Credentials"});
+        }
+     }
+     catch(err){
+        res.send({"message":"Something Went Wrong","error":err.message});
+     }
+})
 
 //get all users
 userRoute.get("/all",async (req,res) => {
